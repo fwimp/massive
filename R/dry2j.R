b56b37018e771de9594d5c567c9ed487d3eab474
Francis Windram <francis.windram17@imperial.ac.uk>
2025-02-01 15:21:00 +0000

    Remove rlang as a dependency and fix references within docs

--- R/dry2j.R
+++ R/dry2j.R
@@ -5,7 +5,7 @@
 #'
 #' @author Francis Windram
 #' @return The equivalent energy in Joules
-#' @references Peters, R.H. (1983) The Ecological Implications of Body Size. Cambridge: Cambridge University Press (Cambridge Studies in Ecology), pp. 231–296. [https://doi.org/10.1017/CBO9780511608551], Appendix Ia
+#' @references Peters, R.H. (1983) The Ecological Implications of Body Size. Cambridge: Cambridge University Press (Cambridge Studies in Ecology), pp. 231–296. [https://doi.org/10.1017/CBO9780511608551](https://doi.org/10.1017/CBO9780511608551), Appendix Ia
 #'
 #' @examples
 #' v <- rnorm(20, 10, 2)
--- R/dry2wet.R
+++ R/dry2wet.R
@@ -2,10 +2,11 @@
 #' @description This performs a simple conversion of ash dry weight to wet weight via Peters' approximate conversion.
 #'
 #' @param dry A vector of dry masses in kg
+#' @param fctr a factor between 3 and 10 to use in the conversion (as Peters specifies a range of possible conversions).
 #'
 #' @author Francis Windram
 #' @return The equivalent wet mass values in kg
-#' @references Peters, R.H. (1983) The Ecological Implications of Body Size. Cambridge: Cambridge University Press (Cambridge Studies in Ecology), pp. 231–296. [https://doi.org/10.1017/CBO9780511608551], Appendix Ia
+#' @references Peters, R.H. (1983) The Ecological Implications of Body Size. Cambridge: Cambridge University Press (Cambridge Studies in Ecology), pp. 231–296. [https://doi.org/10.1017/CBO9780511608551](https://doi.org/10.1017/CBO9780511608551), Appendix Ia
 #'
 #' @examples
 #' v <- rnorm(20, 10, 2)
